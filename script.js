d1589a49323fc33edee8687e650e6f50cf7d8f23
Philip Robbins <plrobbins68@outlook.com>
2020-08-21 12:02:42 -0400

    Adding JS code for local storage

---
+++ script.js
@@ -0,0 +1,24 @@
+var startDate = moment().format('M/DD/YYYY');
+var dayOne = moment().add(1, 'days').format('M/DD/YYY');
+var dayTwo = moment().add(1, 'days').format('M/DD/YYY');
+var dayThree = moment().add(1, 'days').format('M/DD/YYY');
+var dayFour = moment().add(1, 'days').format('M/DD/YYY');
+var dayFive = moment().add(1, 'days').format('M/DD/YYY');
+
+$(document).ready(function() {
+    console.log("start!");
+
+// On-click
+$("basic-text1").on("click", function(event){
+    event.preventDefault();
+    var enterCity = $("#input").val();
+    var allCities = [];
+
+    allCities = JSON.parse(localStorage.getItem("allCities")) || [];
+    allCities.push(enterCity);
+    localStorage.setItem("allCities", JSON.stringify(allCities));
+
+    showWeather(enterCity);
+})
+})
+
